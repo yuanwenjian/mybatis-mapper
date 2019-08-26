8621f9ced08ac41da1914edda142e55be39d31c1
yuanwj <yuanwj0929@163.com>
2019-08-26 18:29:15 +0800

    添加测试代码

--- src/test/java/com/github/MybatisMapperTest.java
+++ src/test/java/com/github/MybatisMapperTest.java
@@ -1,84 +1,23 @@
-Mybatis-mapper用于统一构建条件查询
+package com.github;
 
-使用方法如下
+import com.github.condition.Criteria;
+import com.github.condition.JoinMapperCondition;
+import com.github.condition.MapperCondition;
+import demo.mapper.PhoneImeiMapper;
+import demo.model.PhoneImei;
+import demo.model.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Before;
+import org.junit.Test;
 
-1.引入pom文件
-```xml
-<dependency>
-    <groupId>com.github</groupId>
-        <artifactId>mybatis-mapper</artifactId>
-    <version>1.0-SNAPSHOT</version>
-</dependency>
-```
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
-2.java调用
-
-- 单表查询
-xml 配置
-```xml
-    <select id="findByCondition"
-            parameterType="com.github.condition.MapperCondition"
-            resultType="demo.model.PhoneImei">
-        SELECT
-        <if test="distinct">
-            distinct
-        </if>
-        *
-        FROM wms_phone_imei
-        <if test="_parameter!=null">
-            <!--通用sql解析-->
-            <include refid="com.github.common.mapperCondition"></include> 
-        </if>
-        <if test="orderByClause !=null">
-            order by ${orderByClause}
-        </if>
-        <if test="offset !=null and size !=null">
-            LIMIT ${offset},${size}
-        </if>
-
-    </select>
-```
-
-java调用
-```
-PhoneImeiMapper mapper = sqlSession.getMapper(PhoneImeiMapper.class); // 通过session获取mapper
-
-MapperCondition mapperCondition = new MapperCondition();
-Criteria criteria = mapperCondition.createCriteria(PhoneImei.class);
-List<Long> ids = new ArrayList<>();
-ids.add(12l);
-ids.add(13l);
-ids.add(14l);
-criteria.andIn("imeiId",ids);
-List<PhoneImei> phoneImeiList = mapper.findByCondition(mapperCondition);
-```
-
-
-- 关联查询
-xml 配置
-```xml
-    <select id="joinMapperCondition" parameterType="com.github.condition.JoinMapperCondition"
-            resultType="demo.model.PhoneImei">
-        SELECT 
-        <if test="distinct">
-                    distinct
-        </if>
-        phoneImei.* FROM wms_phone_imei phoneImei
-        JOIN wms_user user ON phoneImei.imei_id=user.user_id
-        <if test="_parameter!=null">
-            <include refid="com.github.common.mapperCondition"></include>
-        </if>
-
-        <if test="orderByClause !=null">
-            order by ${orderByClause}
-        </if>
-        <if test="offset !=null and size !=null">
-            LIMIT ${offset},${size}
-        </if>
-    </select>
-```
-
-```java
 public class MybatisMapperTest {
 
 
@@ -103,8 +42,6 @@ public class MybatisMapperTest {
     @Test
     /**
      * 单表多条件查询 and
-     * ==>  Preparing: SELECT * FROM wms_phone_imei WHERE ( imei_id in ( ? , ? , ? ) AND tenant_id= ? ) 
-     *   ==> Parameters: 12(Long), 13(Long), 14(Long), 111(Integer)
      */
     public void testAnd() {
         MapperCondition mapperCondition = new MapperCondition();
@@ -115,7 +52,6 @@ public class MybatisMapperTest {
         ids.add(14l);
         criteria.andIn("imeiId",ids);
         criteria.andEqual("tenantId",111);
-
         List<PhoneImei> phoneImeiList = mapper.findByCondition(mapperCondition);
 
     }
@@ -123,8 +59,6 @@ public class MybatisMapperTest {
     @Test
     /**
      * 单表or查询
-     * ==>  Preparing: SELECT * FROM wms_phone_imei WHERE ( imei_id= ? ) or( tenant_id= ? ) 
-     * ==> Parameters: 14(Integer), 50001(Integer)
      */
     public void testOr() {
         MapperCondition mapperCondition = new MapperCondition();
@@ -139,8 +73,6 @@ public class MybatisMapperTest {
     @Test
     /**
      * 关联查询 and查询
-     * ==>  Preparing: SELECT phoneImei.* FROM wms_phone_imei phoneImei JOIN wms_user user ON phoneImei.imei_id=user.user_id WHERE ( user.user_id= ? AND phoneImei.imei_id= ? ) 
-     * ==> Parameters: 15(Integer), 14(Integer)
      */
     public void testJoinAnd() {
         JoinMapperCondition mapperCondition = new JoinMapperCondition();
@@ -154,8 +86,6 @@ public class MybatisMapperTest {
     @Test
     /**
      * 关联查询 or
-     * ==>  Preparing: SELECT distinct phoneImei.* FROM wms_phone_imei phoneImei JOIN wms_user user ON phoneImei.imei_id=user.user_id WHERE ( phoneImei.imei_id= ? ) or( user.user_id= ? AND user.user_name= ? ) LIMIT 1,20 
-     * ==> Parameters: 14(Integer), 15(Integer), aa(String)
      */
     public void testJoinOr() {
 
@@ -173,8 +103,6 @@ public class MybatisMapperTest {
     @Test
     /**
      * 排序查询
-     * ==>  Preparing: SELECT phoneImei.* FROM wms_phone_imei phoneImei JOIN wms_user user ON phoneImei.imei_id=user.user_id WHERE ( phoneImei.imei_id= ? ) or( user.user_id= ? AND user.user_name= ? ) order by phoneImei.imei_id asc 
-     * ==> Parameters: 14(Integer), 15(Integer), aa(String)
      */
     public void testOrder() {
         JoinMapperCondition mapperCondition = new JoinMapperCondition();
@@ -187,4 +115,3 @@ public class MybatisMapperTest {
         mapper.joinMapperCondition(mapperCondition);
     }
 }
-```
