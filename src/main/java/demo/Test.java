8b1915a7f0037e4bb756a564d19d53e802384beb
yuanwj <yuanwj0929@163.com>
2019-08-26 16:43:29 +0800

    分支提交测试

--- src/main/java/demo/Test.java
+++ src/main/java/demo/Test.java
@@ -1,9 +1,11 @@
 package demo;
 
 import com.github.condition.Criteria;
+import com.github.condition.JoinMapperCondition;
 import com.github.condition.MapperCondition;
 import demo.mapper.PhoneImeiMapper;
 import demo.model.PhoneImei;
+import demo.model.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -24,21 +26,29 @@ public class Test {
             SqlSession sqlSession = sqlSessionFactory.openSession(); //获取session
             PhoneImeiMapper mapper = sqlSession.getMapper(PhoneImeiMapper.class); // 通过session获取mapper
 
-            MapperCondition mapperCondition = new MapperCondition();
-            Criteria<PhoneImei> criteria = mapperCondition.createCriteria(PhoneImei.class);
+//            JoinMapperCondition mapperCondition = new JoinMapperCondition();
+//            Criteria<PhoneImei> criteria = mapperCondition.createJoinCriteria(PhoneImei.class);
 //            Criteria<User> userCriteria = mapperCondition.createJoinCriteria(User.class,"user");
+//            mapperCondition.appendOrder("phoneImei","imeiId",true);
+//            mapperCondition.appendOrder(User.class,"userId",true);
 //            Criteria phoneCriteria = mapperCondition.or(PhoneImei.class);
-            Criteria phoneCriteria = mapperCondition.or(PhoneImei.class);
-            phoneCriteria.andEqual("imeiNum", "868498020093495");
-            phoneCriteria.andEqual("tenantId",50001);
+//            Criteria phoneCriteria = mapperCondition.or(PhoneImei.class);
+//            phoneCriteria.andEqual("imeiNum", "868498020093495");
+//            phoneCriteria.andEqual("tenantId",50001);
+//
+            MapperCondition mapperCondition = new MapperCondition();
+            Criteria criteria = mapperCondition.createCriteria(PhoneImei.class);
+            mapperCondition.appendOrderByFiled("imeiId");
+
             List<Long> ids = new ArrayList<>();
             ids.add(12l);
             ids.add(13l);
             ids.add(14l);
             criteria.andIn("imeiId",ids);
 //            userCriteria.andEqual("userId",23);
-            List<PhoneImei> phoneImeiList = mapper.joinCondition(mapperCondition); //查询
-
+//            List<PhoneImei> phoneImeiList = mapper.joinCondition(mapperCondition); //查询
+//            List<PhoneImei> phoneImeiList = mapper.joinMapperCondition(mapperCondition);
+            List<PhoneImei> phoneImeiList = mapper.findByCondition(mapperCondition);
             System.out.println(phoneImeiList.size());
         } catch (IOException e) {
             e.printStackTrace();
